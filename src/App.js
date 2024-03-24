8bd2cef550f252bf42add695505e6cc5c99c7887
Mitchelle M <shanellmusarurwa6@gmail.com>
2024-03-24 15:24:39 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
-     
-    </div>
+       <div className="container">
+          <Weather defaultCity="Pretoria" />
+          <footer>
+            This project was coded by Mitchelle Musarurwa and is on{" "}
+            <a
+              href="https://github.com/shanellmusarurwa/my-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on Github
+            </a>
+          </footer>
+        </div>
+      </div>
+    </>  
   );
 }
-
+  
 export default App;
---
+++ src/WeatherInfo.js
@@ -0,0 +1,36 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherInfo(props){
+    return(
+        <div className="WeatherInfo">
+            <h1> {props.data.city}</h1>
+        <ul>
+          <li><FormattedDate date={props.data.date}/></li>
+          <li className="text-capitalize">{props.data.description}</li>
+          </ul>
+          <div className="row mt-3">
+            <div className="col-6">
+              <div className="clearfix">
+                <div className="float-left">
+                <WeatherIcon code={props.data.icon} />
+                </div>
+                <div className="float-left">
+                  <span className="temperature">
+                     {Math.round(props.data.temperature)}
+                  </span>
+                  <span className="units">ºC</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {props.data.humidity}%</li>
+                <li>Wind: {props.data.wind} km/h</li>
+              </ul>
+            </div>
+            </div>
+        </div>
+    );
+}
