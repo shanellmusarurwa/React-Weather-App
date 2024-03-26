6a73d92c442467cdc55b5db09bce4be62d09c7f6
Mitchelle M <shanellmusarurwa6@gmail.com>
2024-03-26 14:13:14 +0200

    updated apiUrl

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props) {
 
   function search(){
     const apiKey = "5293d8454b519c30f6f6331f38c85b4c";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
